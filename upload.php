c4efef78eb2f2a1ea5ff47ab3c1f41e1eaff2842
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-01 18:34:33 -0800

    test

--- upload.php
+++ upload.php
@@ -43,7 +43,7 @@ ini_set('display_errors', 1);
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
       echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
       // REDIRECT
-      header("Location: " . $_SERVER['REQUEST_URI']);
+      // header("Location: " . $_SERVER['REQUEST_URI']);
     } else {
       echo "Sorry, there was an error uploading your file.";
     }
