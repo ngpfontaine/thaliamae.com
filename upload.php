083c266d0dd658deb65b985b8617c0d493739bba
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-01 17:03:16 -0800

    file size

--- upload.php
+++ upload.php
@@ -21,7 +21,7 @@
     $uploadOk = 0;
 	}
 	// Check file size
-	if ($_FILES["file"]["size"] > 500000) {
+	if ($_FILES["file"]["size"] > 2000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 	}
