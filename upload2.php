c9a8e36aa3b3f13bd996e1a183552ce47986e4f0
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-05 12:51:03 -0800

    upload and compresse multiple files

--- upload2.php
+++ upload2.php
@@ -24,6 +24,7 @@ function compress_image($source_url, $destination_url, $quality) {
 
 if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
         // Loop $_FILES to execute all files
+    echo $_FILES['files']['name'];
     foreach ($_FILES['files']['name'] as $f => $name) {     
         echo 'foreach';
         if ($_FILES['files']['error'][$f] != 0) {
