d8bb3ffc58a26f8999747f28cbaffe0b03e52d72
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-05 12:30:28 -0800

    upload and compresse multiple files

--- upload2.php
+++ upload2.php
@@ -26,15 +26,18 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
         // Loop $_FILES to execute all files
     foreach ($_FILES['files']['name'] as $f => $name) {     
         if ($_FILES['files']['error'][$f] != 0) {
+            echo 'error found';
             continue; // Skip file if any error found
         }
         if ($_FILES['files']['error'][$f] == 0) {
             if ($_FILES['files']['size'][$f] > $max_file_size ) {
                 $message[] = "$name is too large!";
+                echo 'too large';
                 continue; // Skip large files.
             }
             elseif( ! in_array(pathinfo($name, PATHINFO_EXTENSION), $valid_formats) ){
                 $message[] = "$name is not a valid format";
+                echo 'not valid format';
                 continue; // Skip invalid file formats
             }
             else{ // No error found! Move uploaded files 
@@ -42,6 +45,7 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
                 if(is_uploaded_file($_FILES["files"]["tmp_name"][$f])) {
                     //Add a '.jpg' to the name because I'm lazy.
                     compress_image($_FILES["files"]["tmp_name"][$f], $path.basename($name).'.jpg', 90);
+                    echo 'compress';
                     $count ++; // Number of successfully uploaded files
                     // REDIRECT
                     header("HTTP/1.1 303 See Other");
