488087f6e9e1c253e272aa902fc86e474e2fd8df
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-08 21:29:53 -0800

    rezising working

--- upload2.php
+++ upload2.php
@@ -11,6 +11,7 @@ $name_array = $_FILES['files']['name'];
 $tmp_name_array = $_FILES['files']['tmp_name'];
 // Number of files
 $count_tmp_name_array = count($tmp_name_array);
+echo 'filessss';
 echo $count_tmp_name_array;
 
 // We define the static final name for uploaded files (in the loop we will add an number to the end)
