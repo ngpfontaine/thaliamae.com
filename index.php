c6491e074c6da39f3420956d38462ed55c936bb7
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-09 14:08:09 -0800

    link text select

--- index.php
+++ index.php
@@ -58,8 +58,8 @@
 				<!-- <h4>August 2012 - Feb. 18 2017</h4> -->
 				<h4>Heading</h4>
 			</div><div class='nav-right clear-fix'>
-				<span id='btn-about' class='link'>ABOUT</span>
-				<span class='btn' id='btn-upload'>UPLOAD <i class="fa fa-upload"></i></span>
+				<span id='btn-about' class='link noselect'>ABOUT</span>
+				<span class='btn' id='btn-upload noselect'>UPLOAD <i class="fa fa-upload"></i></span>
 			</div>
 			<div class='clear-fix'></div>
 		</nav>
