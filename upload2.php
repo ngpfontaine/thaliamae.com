0fa8fb987091163a113a77653ff3250ec2156cff
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-10 14:21:19 -0800

    upload not working, interlace issue?

--- upload2.php
+++ upload2.php
@@ -93,6 +93,46 @@ function resizeImage($sourceImage, $targetImage, $maxWidth, $maxHeight, $quality
   return true;
 }
 
+function resize($file, $imgpath, $width, $height){
+    /* Get original image x y*/
+    list($w, $h) = getimagesize($file['tmp_name']);
+    /* calculate new image size with ratio */
+    $ratio = max($width/$w, $height/$h);
+    $h = ceil($height / $ratio);
+    $x = ($w - $width / $ratio) / 2;
+    $w = ceil($width / $ratio);
+
+    /* new file name */
+    $path = $imgpath;
+    /* read binary data from image file */
+    $imgString = file_get_contents($file['tmp_name']);
+    /* create image from string */
+    $image = imagecreatefromstring($imgString);
+    $tmp = imagecreatetruecolor($width, $height);
+    imagecopyresampled($tmp, $image, 0, 0, $x, 0, $width, $height, $w, $h);
+    /* Save image */
+    switch ($file['type']) {
+       case 'image/jpeg':
+          imagejpeg($tmp, $path, 100);
+          break;
+       case 'image/png':
+          imagepng($tmp, $path, 0);
+          break;
+       case 'image/gif':
+          imagegif($tmp, $path);
+          break;
+          default:
+          //exit;
+          break;
+        }
+     return $path;
+
+     /* cleanup memory */
+     imagedestroy($image);
+     imagedestroy($tmp);
+}
+
+
 if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
   
   // TRIM SPACES INTO FILES ARRAY
@@ -154,45 +194,6 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
   // REDIRECT
   // header("HTTP/1.1 303 See Other");
   // header("Location: https://$_SERVER[HTTP_HOST]/");
-}
-
-function resize($file, $imgpath, $width, $height){
-    /* Get original image x y*/
-    list($w, $h) = getimagesize($file['tmp_name']);
-    /* calculate new image size with ratio */
-    $ratio = max($width/$w, $height/$h);
-    $h = ceil($height / $ratio);
-    $x = ($w - $width / $ratio) / 2;
-    $w = ceil($width / $ratio);
-
-    /* new file name */
-    $path = $imgpath;
-    /* read binary data from image file */
-    $imgString = file_get_contents($file['tmp_name']);
-    /* create image from string */
-    $image = imagecreatefromstring($imgString);
-    $tmp = imagecreatetruecolor($width, $height);
-    imagecopyresampled($tmp, $image, 0, 0, $x, 0, $width, $height, $w, $h);
-    /* Save image */
-    switch ($file['type']) {
-       case 'image/jpeg':
-          imagejpeg($tmp, $path, 100);
-          break;
-       case 'image/png':
-          imagepng($tmp, $path, 0);
-          break;
-       case 'image/gif':
-          imagegif($tmp, $path);
-          break;
-          default:
-          //exit;
-          break;
-        }
-     return $path;
-
-     /* cleanup memory */
-     imagedestroy($image);
-     imagedestroy($tmp);
 }
 
 ?>
