cdc422ae00cd8e4605cbdf5d6ad5ffeda276a6e6
Nic Fontaine <ngpfontaine@gmail.com>
2017-03-12 17:55:51 -0700

    paw pagin links

--- js/main.js
+++ js/main.js
@@ -171,7 +171,7 @@ for (var i=0; i<(pagesNo-1); i++) {
   var itm = document.getElementById("page-paw-source").lastChild;
   var cln = itm.cloneNode(true);
   contPagin.appendChild(cln);
-  contPagin.getElementsByTagName('i')[i].setAttribute('href','https://thaliamae.com?p=' + (pagesNo-1));
+  // contPagin.getElementsByTagName('i')[i].setAttribute('href','https://thaliamae.com?p=' + (pagesNo-1));
   console.log('running paw pagin loop - ' + i);
   if (i == Number(paramP)) {
     cln.classList.add('active');
